dc332649783634e784941d07322321bc7dbe776e
Vadym Tyemirov <temirov@users.noreply.github.com>
2025-09-25 13:25:16 -0700

    Update NoteRecord attachments optional syntax

--- store.js
+++ store.js
@@ -10,7 +10,7 @@ import { collectReferencedAttachments, sanitizeAttachmentDictionary } from "./ui
  * @property {string} updatedAtIso
  * @property {string} lastActivityIso
  * @property {object=} classification
- * @property {Record<string, { dataUrl: string, altText: string }>=} attachments
+ * @property {Record<string, { dataUrl: string, altText: string }>=} [attachments]
  */
 
 export const GravityStore = (() => {
